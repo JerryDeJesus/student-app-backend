2cd5ae526688040e3e2bcb3647a15fe52f385b56
Jerry De Jesus <jdejesus7@live.com>
2022-06-27 13:13:24 -0400

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const studentsController = require('./controllers/studentsController');
 const namesController = require('./controllers/namesController');
 
+app.use(cors());
 app.use('/students', studentsController);
 app.use('/names', namesController);
 
